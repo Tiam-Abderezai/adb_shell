91af55987673806b8f98fc8bc82e05e72c4696b8
Tiam Abderezai <tiam.abderezai@gmail.com>
2022-04-17 15:12:11 -0400

    Initial commit

---
+++ adb.py
@@ -0,0 +1,28 @@
+import os
+from ppadb.client import Client as AdbClient
+
+username = "\\admin@email.com"
+password = "password"
+clientDevice = "<CLIENT DEVICE>"
+clientIP = "127.0.0.1"
+clientPort = 5037
+
+def execADB():
+    # Default is "127.0.0.1" and 5037
+    client = AdbClient(clientIP, clientPort)
+    # Pass arg device name (use 'adb devices' to get device name)
+    device = client.device(clientDevice)
+    # Enter 'hello world' text into EditText
+    device.shell(f"input text {username}")
+    # keyevent KEYCODE_TAB twice to go to next EditText
+    device.shell(f"input keyevent KEYCODE_TAB")
+    device.shell(f"input keyevent KEYCODE_TAB")
+    # enter password
+    device.shell(f"input text {password}")
+    device.shell(f"input keyevent KEYCODE_TAB")
+    device.shell(f"input keyevent KEYCODE_TAB")
+
+    # device.shell(f"input text ${password}")
+
+if __name__ == '__main__':
+    execADB()
---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+
+def execADB():
+    os.system('adb shell input text tiam')
+
+if __name__ == '__main__':
+    execADB()
